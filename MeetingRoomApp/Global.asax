c9e619c7fda18241d7bb49cdca7d33e9d89f86a1
Matt <matthew.thompson.a@gmail.com>
2017-02-20 12:17:29 -0500

    Add project files.

---
+++ MeetingRoomApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeetingRoomApp.MvcApplication" Language="C#" %>
---
+++ MeetingRoomApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetingRoomApp.Startup))]
+namespace MeetingRoomApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
